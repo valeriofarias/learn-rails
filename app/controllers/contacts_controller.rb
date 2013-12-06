1c95ca27e8be02c8e1f389dba5067c2b15053630
valeriofarias <valeriofc@gmail.com>
2013-12-06 11:14:31 -0300

    save data to a spreadsheet

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,7 @@ class ContactsController < ApplicationController
 	def create
 		@contact = Contact.new(secure_params)
 		if @contact.valid?
+			@contact.update_spreadsheet
 			flash[:notice] = "Message sent from #{@contact.name}."
 			redirect_to root_path
 		else
@@ -14,7 +15,7 @@ class ContactsController < ApplicationController
 	end
 
 	private 
-	
+
 	def secure_params
 		params.require(:contact).permit(:name, :email, :content)
 	end
